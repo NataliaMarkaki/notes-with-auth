4a8717947879caf783cc734a5f91d49cf9ce24fa
NataliaMarkaki <markaki.natalia@gmail.com>
2026-06-19 08:41:50 +0100

    feat: add basic set up

---
+++ app/page.tsx
@@ -0,0 +1,9 @@
+export const revalidate = 30;
+
+export default async function Home() {
+  return (
+    <div className='min-h-screen bg-zinc-50'>
+      <main className='max-w-2xl mx-auto px-4 py-8'>hello</main>
+    </div>
+  );
+}
